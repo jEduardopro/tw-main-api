30639fe4a98af7286fb34123eb298967bf07af23
Jesus Eduardo <lalo.codein22@gmail.com>
2023-07-15 12:01:36 -0600

    fix date_birth in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'country' => $this->faker->country(),
-            'date_birth' => $this->faker->date()
+            'date_birth' => $this->faker->dateTimeBetween('-30 years', '-14 years')
         ];
     }
 
