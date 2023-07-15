a653bef7a1ef96141af2f9128b1f8ac1f736d61a
Jesus Eduardo <lalo.codein22@gmail.com>
2023-07-15 11:44:58 -0600

    remove public media to fresh tweet and user collections

--- docker/scripts/fresh-db.sh
+++ docker/scripts/fresh-db.sh
@@ -7,6 +7,9 @@ php artisan migrate:fresh
 echo "artisan passport:install"
 php artisan passport:install
 
+rm -rf public/media/tweet
+rm -rf public/media/user
+
 echo "artisan db:seed"
 php artisan db:seed
 
